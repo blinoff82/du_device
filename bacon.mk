b561ba9a8f3871670381d3192d4b1fd5974bf6e1
Ivan Blinov <blinov.in@gmail.com>
2017-01-16 09:46:40 +0300

    Added Snapdragon camera

--- bacon.mk
+++ bacon.mk
@@ -68,12 +68,12 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
+    SnapdragonCamera \
     camera.bacon \
     libshim_camera
     
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.codecremote=false \
-    Snap
 
 # Charger
 PRODUCT_PACKAGES += \
