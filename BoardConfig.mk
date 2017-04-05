77a217b8a404365c991fd9006173d4fd891d2646
Caleb Xu <calebcenter@live.com>
2017-04-05 10:41:03 -0400

    Revert "bacon: Compile libhwui for performance"
    clang does not support -mapcs after upgrading to 7.1.2, for whatever reason.
    Maybe we can try again later.
    
    This reverts commit 025e2da6c0a8162c65c1b3e2fa11fe08082142b0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,9 +171,6 @@ TARGET_RIL_VARIANT := caf
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.bacon
 WITH_LZMA_OTA := true
 
-# Compile libhwui in performance mode
-HWUI_COMPILE_FOR_PERF := true
-
 # RPC
 TARGET_NO_RPC := true
 
