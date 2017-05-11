1b1f42b70574404972000c7373ee42589442aa55
Caleb Xu <calebcenter@live.com>
2017-05-11 13:44:27 -0400

    bacon: enable real time lockscreen charging info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,6 +209,9 @@ TARGET_LDPRELOAD := libNimsWrap.so
 endif
 endif
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 #Snapdragon LLVM Compiler
 ifneq ($(HOST_OS),darwin)
 SDCLANG := true
